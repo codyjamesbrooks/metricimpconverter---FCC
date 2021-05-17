40eac4b31c9235e3d795b280a1407aa902600c5d
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-16 20:35:01 -0600

    added converHandler logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,48 @@
 function ConvertHandler() {
+  this.inputRegex = /^(\d*\.?\d*\/?\d*\.?\d*)[ ]*(\w*)?/;
+
   this.getNum = function (input) {
-    const numberRegx = /\d*[.\/]*\d*/;
-    let result = eval(input.match(numberRegx)[0]);
-    return result;
+    let result = input.match(this.inputRegex)[1];
+    return eval(result);
   };
 
   this.getUnit = function (input) {
-    let result;
-
+    let result = input.match(this.inputRegex)[2];
     return result;
   };
 
   this.getReturnUnit = function (initUnit) {
-    let result;
-
-    return result;
+    switch (initUnit) {
+      case "gal":
+        return "L";
+      case "L":
+        return "gal";
+      case "mi":
+        return "km";
+      case "km":
+        return "mi";
+      case "lbs":
+        return "kg";
+      case "kg":
+        return "lbs";
+    }
   };
 
   this.spellOutUnit = function (unit) {
-    let result;
-
-    return result;
+    switch (unit) {
+      case "gal":
+        return "gallons";
+      case "L":
+        return "liters";
+      case "mi":
+        return "miles";
+      case "km":
+        return "kilometers";
+      case "lbs":
+        return "pounds";
+      case "kg":
+        return "kilograms";
+    }
   };
 
   this.convert = function (initNum, initUnit) {
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,12 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
   app.route("/api/convert").get((req, res) => {
-    res.json({ initNum: convertHandler.getNum(req.query.input) });
+    res.json({
+      initNum: convertHandler.getNum(req.query.input),
+      initUnit: convertHandler.getUnit(req.query.input),
+      returnUnit: convertHandler.getReturnUnit(
+        convertHandler.getUnit(req.query.input)
+      ),
+    });
   });
 };
